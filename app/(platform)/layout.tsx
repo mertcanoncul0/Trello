952068e25f75ca048003976d412ee96e1993e522
Mertcan Öncül <mertcanoncul@Mertcan-MacBook-Air.local>
2024-06-30 07:33:34 +0300

    initial commit

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,19 @@
+import { ModalProvider } from "@/components/providers/modal.provider";
+import { ClerkProvider } from "@clerk/nextjs"
+import { Toaster } from "sonner";
+
+const PlatformLayout = ({
+    children
+}: {
+    children: React.ReactNode
+}) => {
+    return (
+        <ClerkProvider>
+            <Toaster />
+            <ModalProvider />
+            {children}
+        </ClerkProvider>
+    );
+};
+
+export default PlatformLayout
